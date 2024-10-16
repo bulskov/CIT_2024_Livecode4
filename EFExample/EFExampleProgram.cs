e90d2b0a86f8c0ada28758c5eea018f4769a6658
bulskov <bulskov@ruc.dk>
2024-10-16 10:43:50 +0200

    first EF example

---
+++ EFExample/EFExampleProgram.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EFExample;
+
+
+
+var context = new NorthwindContext();
+var categories = context.Categories.Where(x => x.Id < 5).ToList();
+
+foreach(var e in categories)
+{
+    Console.WriteLine(e.Name);
+}
---
+++ EFExample/NorthwindContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFExample;
+internal class NorthwindContext : DbContext
+{
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+        optionsBuilder.UseNpgsql("host=localhost;db=northwind;uid=bulskov;pwd=henrik");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().ToTable("categories");
+        modelBuilder.Entity<Category>().Property(x => x.Id).HasColumnName("categoryid");
+        modelBuilder.Entity<Category>().Property(x => x.Name).HasColumnName("categoryname");
+        modelBuilder.Entity<Category>().Property(x => x.Description).HasColumnName("description");
+    }
+}
