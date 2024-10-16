5cfcb49ba8ccb0bc1ebcef423df6c61c9c5331e8
bulskov <bulskov@ruc.dk>
2024-10-16 08:48:49 +0200

    connect to postgres

--- Livecode4/ADOProgram.cs
+++ Livecode4/ADOProgram.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Npgsql;
+
+
+
+var connectionString = "host=localhost;db=northwind;uid=bulskov;pwd=henrik";
+
+
+using var connection = new NpgsqlConnection(connectionString);
+connection.Open();
+
+using var cmd = new NpgsqlCommand();
+
+cmd.Connection = connection;
+cmd.CommandText = "select * from categories";
+
+using var reader = cmd.ExecuteReader();
+
+while(reader.Read())
+{
+    Console.WriteLine(reader.GetInt32(0));
+}
